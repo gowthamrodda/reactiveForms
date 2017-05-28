a98e14f6cbb6f1a9770fc0cd7750aed2dc037db9
Gowtham Kumar <gowtham@olivetech.net>
2017-05-28 14:24:08 +0530

    added user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {
     path: '',
+    children: []
+  },
+  {
+    path: 'User',
+    component: UserComponent,
     children: []
   }
 ];
